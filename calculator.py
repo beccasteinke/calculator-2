2f598cfc019408d883101b566c4c6a0c58e5f698
vagrant <vagrant@vagrant.vm>
2020-09-30 20:08:10 +0000

    Finished equations

--- calculator.py
+++ calculator.py
@@ -5,3 +5,47 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 
 
 # Replace this with your code
+
+#ask for input; use input to split into a list
+#input: + 1 2 -> ["+", "3", "2"]
+# + equation[0] 3 equation [1]
+while True:
+    input_string = input("Enter your equation > ")
+    equation = input_string.split(' ')
+
+    if equation[0] == "q":
+        break
+
+    #add
+    if equation[0] == "+":
+        result = equation[1] + equation[2]
+        print(result)
+
+    #subtract
+    if equation[0] == "-":
+        result = equation[1] - equation[2]
+        print(result)
+    #multiply
+    if equation[0] == "*":
+        result = equation[1] * equation[2]
+        print(result)
+    #divide
+    if equation[0] == "/":
+        result = equation[1] / equation[2]
+        print(result)
+    #square
+    if equation[0] == "square":
+        result = equation[1] ** 2
+        print(result)
+    #cube
+    if equation[0] == "cube":
+        result = equation[1] ** 3
+        print(result)
+    #power
+    if equation[0] == "pow":
+        result = equation[1] ** equation[2]
+        print(result)
+    #mod
+    if equation[0] == "mod"
+        result = equation[1] % equation[2]
+        print(result)
